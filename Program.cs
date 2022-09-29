efd6af6b2360a82c1417000ab85b6ce700a361f9
Natalia <wolkowan@yandex.ru>
2022-09-29 17:56:44 +0300

    hw

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+string[] GetArray()
+{
+    Console.WriteLine("Введите массив строк через пробел");
+            
+    string[] original = Console.ReadLine().Split(' ');
+    string[] result = new string[original.Length];
+    int i = 0;
+            
+    foreach (string value in original)
+        {
+            if (value.Length <= 3)
+            {
+                result[i] = value;
+                i++;
+            }
+        }
+    return result;
+}
+
+
+
+
+
+void PrintArray(string[] matr)
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                System.Console.Write($"{matr[i]} ");
+            }  
+    }
+
+PrintArray(GetArray());
+          
+
